d736ba95e2bdd2a907b71682a3b6d66d228376f4
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2025-01-01 00:15:57 +0100

    nav

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-// Get the hamburger menu and nav menu
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.nav-menu');
 
-// Add click event to toggle menu visibility with transition
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active'); // Toggle active class for animation
-  navMenu.classList.toggle('show'); // Toggle the menu visibility
+  hamburgerMenu.addEventListener('click', () => {
+    console.log("clicked")
+    hamburgerMenu.classList.toggle('active'); // Toggle active class for animation
+    navMenu.classList.toggle('show'); // Toggle the menu visibility
+  });
 });
